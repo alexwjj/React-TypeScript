65c86e4f26d7b2c4f7c309bd70bca7fffe266d7e
aleexwjj <wujj@wedoctor.com>
2021-03-02 17:58:24 +0800

    fix: craco改动

--- craco.config.js
+++ craco.config.js
@@ -1,18 +1,27 @@
 const CracoLessPlugin = require('craco-less');
+const path = require('path')
+
+const pathResolve = pathUrl => path.join(__dirname, pathUrl)
 
 module.exports = {
-  
-	plugins: [
-		{
-			plugin: CracoLessPlugin,
-			options: {
-				lessLoaderOptions: {
-					lessOptions: {
-						modifyVars: { '@primary-color': '#3e7fee' },
-						javascriptEnabled: true,
+	webpack: {
+		alias: {
+			'@': pathResolve('src'),
+			'@assets': pathResolve('src/assets'),
+			'@components': pathResolve('src/components'),
+		}
+	},
+	plugins: [{
+		plugin: CracoLessPlugin,
+		options: {
+			lessLoaderOptions: {
+				lessOptions: {
+					modifyVars: {
+						'@primary-color': '#3e7fee'
 					},
+					javascriptEnabled: true,
 				},
 			},
 		},
-	],
+	}, ],
 };
