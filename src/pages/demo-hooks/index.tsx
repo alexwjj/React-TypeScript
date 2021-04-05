a1ba4676f5d286f8697c702cb75030ba2c037bad
aleexwjj <wujj@wedoctor.com>
2021-04-05 12:26:52 +0800

    feat: useCallback用法

--- src/pages/demo-hooks/index.tsx
+++ src/pages/demo-hooks/index.tsx
@@ -1,30 +1,30 @@
-import './index.less';
-import React, { useState, useEffect } from 'react';
+import "./index.less";
+import React, { useState, useEffect } from "react";
+import { Alert } from "zent";
+import CallbackDemo from "./useCallback";
 
-function Example() {
+function Hooks() {
   const [count, setCount] = useState(0);
-  const [time, setTime] = useState('2021-03-15');
-
   useEffect(() => {
     document.title = `You clicked ${count} times`;
 
-	return ()=> {
-		console.log('11111');
-	}
-  }, [count, time]);
+    return () => {
+      console.log("11111");
+    };
+  }, [count]);
 
   return (
     <div>
-      <p>今天是{time}</p>
-      <button onClick={() => setTime('3-16')}>
-        next days
-      </button>
+      <Alert
+        title="useCallback/useMemo"
+        description="useCallback主要用于函数的缓存，依赖项不变返回的函数的引用地址不变，uesMemo是针对值的缓存。都是为了避免组件的重复渲染"
+      ></Alert>
+      <CallbackDemo></CallbackDemo>
+      <Alert title="useEffect" description=""></Alert>
       <p>You clicked {count} times</p>
-      <button onClick={() => setCount(count + 1)}>
-        Click me
-      </button>
+      <button onClick={() => setCount(count + 1)}>Click me</button>
     </div>
   );
 }
 
-export default Example;
+export default Hooks;
