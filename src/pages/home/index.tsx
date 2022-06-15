ab2b8710a5f37b110d03ec8c75987c2078296e55
alexjjwu <alexjjwu@tencent.com>
2022-06-15 18:58:39 +0800

    test

--- src/pages/home/index.tsx
+++ src/pages/home/index.tsx
@@ -1,9 +1,51 @@
-import React from 'react';
-import { Alert } from 'zent';
+import { Button, Modal } from "antd";
+import React from "react";
+import { Alert } from "zent";
+import "./index.less";
 
 export const Home: React.FC<any> = () => {
+  const [isModalVisible, setIsModalVisible] = React.useState(false);
+  const showModal = () => {
+    setIsModalVisible(true);
+  };
+
+  const handleOk = () => {
+    setIsModalVisible(false);
+  };
+
+  const handleCancel = () => {
+    setIsModalVisible(false);
+  };
+
   return (
-    <Alert title="俊劫的主页" description="加油加油，冲冲冲！！！"></Alert>
+    <>
+      <Alert title="俊劫的主页" description="加油加油，冲冲冲！！！"></Alert>
+      <Button type="primary" onClick={showModal}>
+        Open Modal
+      </Button>
+      <Modal
+        title=""
+        visible={isModalVisible}
+        onOk={handleOk}
+        wrapClassName="J-Model"
+        onCancel={handleCancel}
+        footer={null}
+        maskClosable={false}
+      >
+        <p>Some contents...</p>
+        <p>Some contents...</p>
+        <p>Some contents...</p>
+        <p>Some contents...</p>
+        <p>Some contents...</p>
+        <p>Some contents...</p>
+        <p>Some contents...</p>
+        <p>Some contents...</p>
+        <p>Some contents...</p>
+        <p>Some contents...</p>
+        <p>Some contents...</p>
+        <p>Some contents...</p>
+      </Modal>
+    </>
   );
 };
 
